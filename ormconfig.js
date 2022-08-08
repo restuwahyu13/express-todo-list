136b808930d207629c88b275915ab041946ce45e
restuwahyu13 <restuwahyu13@gmail.com>
2022-08-08 22:07:42 +0700

    publish files

--- ormconfig.js
+++ ormconfig.js
@@ -1,23 +1,33 @@
 const path = require('path')
 
-const pathEntitiesDir = !['production', 'staging'].includes(process.env.NODE_ENV) ? 'src/entities/*.ts' : 'dist/entities/*.js'
-const pathMigrationDir = !['production', 'staging'].includes(process.env.NODE_ENV) ? 'src/databases/migrations' : 'dist/databases/migrations'
+const pathEntitiesDir = !['production', 'staging'].includes(process.env.NODE_ENV) ? 'src/models' : 'dist/models'
+const pathMigrationDir = !['production', 'staging'].includes(process.env.NODE_ENV) ? 'src/database/migrations' : 'dist/database/migrations'
+const pathSeedDir = !['production', 'staging'].includes(process.env.NODE_ENV) ? 'src/database/seeds' : 'dist/database/seeds'
+const pathFactoryDir = !['production', 'staging'].includes(process.env.NODE_ENV) ? 'src/database/factories' : 'dist/database/factories'
 
 const entitiesDir = path.resolve(process.cwd(), pathEntitiesDir)
 const migrationsDir = path.resolve(process.cwd(), pathMigrationDir)
+const seedsDir = path.resolve(process.cwd(), pathSeedDir)
+const factoriesDir = path.resolve(process.cwd(), pathFactoryDir)
+
+const fileExtension = '/' + (!['production', 'staging'].includes(process.env.NODE_ENV) ? '*.ts' : '*.js')
 
 module.exports = {
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
+  port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [entitiesDir],
-  migrations: [migrationsDir],
-  synchronize: !['production', 'staging'].includes(process.env.NODE_ENV) ? true : false,
-  logger: !['production', 'staging'].includes(process.env.NODE_ENV) ? 'advanced-console' : undefined,
-  logging: !['production', 'staging'].includes(process.env.NODE_ENV) ? true : false,
+  synchronize: ['production', 'staging'].includes(process.env.NODE_ENV) ? false : true,
+  logger: ['production', 'staging'].includes(process.env.NODE_ENV) ? undefined : 'advanced-console',
+  logging: ['production', 'staging'].includes(process.env.NODE_ENV) ? false : true,
+  entities: [entitiesDir + fileExtension],
+  migrations: [migrationsDir + fileExtension],
+  seeds: [seedsDir + fileExtension],
+  factories: [factoriesDir + fileExtension],
+  autoLoadEntities: ['production', 'staging'].includes(process.env.NODE_ENV) ? false : true,
+  keepConnectionAlive: true,
   cli: {
     entitiesDir: entitiesDir,
     migrationsDir: migrationsDir
